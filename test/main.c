8e32dc6be4904661f399eefdf98f912931089827
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-07-11 16:51:51 +0100

    m

--- test/main.c
+++ test/main.c
@@ -5,8 +5,9 @@ int main(void)
 {
 	_printf("Character Test:[%c]\n", 'H');
 	printf("Character Test:[%c]\n", 'H');
-	_printf("String Test:[%s]\n", "I am a string !");
-	printf("String Test:[%s]\n", "I am a string !");
-
+	_printf("String test:[%s]\n", "I am a string test !");
+	printf("String test:[%s]\n", "I am a string !");
+	_printf("Percent:[%%]\n");
+	printf("Percent:[%%]\n");
 	return (0);
 }
